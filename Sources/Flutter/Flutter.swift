7e39da936fa0ee5114b28d677cb00cb2b98223b1
Kilo Loco <kyle@kiloloco.com>
2021-04-01 07:20:46 -0700

    support basic widgets

--- Sources/Flutter/Flutter.swift
+++ Sources/Flutter/Flutter.swift
@@ -12,3 +12,20 @@ public protocol StatefulWidget: Widget {
 }
 
 public protocol WidgetState: ObservableObject {}
+
+public typealias ListView = List
+
+public extension ListView {
+    @inlinable static func builder<Content: Widget>(
+        itemCount: Int,
+        @ViewBuilder builder: @escaping (Int) -> Content
+    ) -> some Widget {
+        ForEach(0 ..< itemCount, content: builder)
+    }
+}
+
+public typealias Row = HStack
+
+public typealias Column = VStack
+
+public typealias Stack = ZStack
